cee7d536b562ca4c8b19c3fc15186a88dcbd96fa
JHW <ruby07025@gmail.com>
2025-07-25 10:16:41 +0900

    bucket실행

--- SpringBootBoard/src/main/java/com/smhrd/board/controller/BoardController.java
+++ SpringBootBoard/src/main/java/com/smhrd/board/controller/BoardController.java
@@ -1,6 +1,5 @@
 package com.smhrd.board.controller;
 
-import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -17,10 +16,17 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
+
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.model.CannedAccessControlList;
+import com.amazonaws.services.s3.model.ObjectMetadata;
+import com.amazonaws.services.s3.model.PutObjectRequest;
+import com.smhrd.board.config.BucketConfig;
 import com.smhrd.board.config.FileUploadConfig;
 import com.smhrd.board.entity.BoardEntity;
 import com.smhrd.board.entity.UserEntity;
 import com.smhrd.board.service.BoardService;
+
 import jakarta.servlet.http.HttpSession;
 
 @Controller
@@ -28,11 +34,14 @@ import jakarta.servlet.http.HttpSession;
 public class BoardController {
 	@Autowired
 	BoardService boardService;
-
+	private final BucketConfig bucketConfig;
+    private final AmazonS3 amazonS3;
     private final FileUploadConfig fileUploadConfig;
 
-    BoardController(FileUploadConfig fileUploadConfig) {
-        this.fileUploadConfig = fileUploadConfig;
+    BoardController(FileUploadConfig fileUploadConfig, BucketConfig bucketConfig, AmazonS3 amazonS3) {
+		this.amazonS3 = amazonS3;
+		this.fileUploadConfig = fileUploadConfig;
+        this.bucketConfig = bucketConfig;
     }
 	
 	//글쓰기 기능
@@ -55,34 +64,21 @@ public class BoardController {
 			//이미지의 고유 이름 부여
 			String file_name = UUID.randomUUID()+ "_" +img_name;
 			//random값_이미지이름
-			
-			//C:/upload 폴더에 저장 할 예정
-			// --> 업로드 할 경로를 변수로 가지고 오기
-			String uploadDir = fileUploadConfig.getUploadDir();
-			
-			//예시- C:/upload/123_1.jpg 로 저장
-			String filePath = Paths.get(uploadDir, file_name).toString();
-			// uploadDir + file_name 으로 작성시 OS에 따라 경로 못잡음
-			
-			// 파일 경로 확인 후 이미지 저장
 			try {
-			    image.transferTo(new File(filePath));
-			    
-			    // 경로를 별도의 변수에 저장
-			    imgPath = "/uploads/" +file_name;
-			    
-			} catch (IllegalStateException e) {
-			    e.printStackTrace();
-			} catch (IOException e) {
-			    e.printStackTrace();
-			};
-			
-		}
+				ObjectMetadata metadata = new ObjectMetadata();
+		        metadata.setContentLength(image.getSize());
+		        metadata.setContentType(image.getContentType());
+
+		        PutObjectRequest request = new PutObjectRequest(bucketConfig.getbucketName(), file_name, image.getInputStream(), metadata)
+		                .withCannedAcl(CannedAccessControlList.PublicRead); // public 접근 허용
+
+		        amazonS3.putObject(request);
+		        imgPath = amazonS3.getUrl(bucketConfig.getbucketName(), file_name).toString();
+			}catch(Exception e){
+				e.printStackTrace();
+			}
 		
-		// DB저장
-		// service 객체를 통해
-		//BoardService -. BoardRepository
-		// save()
+		}	
 		
 		BoardEntity entity = new BoardEntity();
 		entity.setTitle(title);
